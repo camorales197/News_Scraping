71c1100cbcd23b910496b5719fd0a22ced603b05
Carlos <carlos_cc_13@hotmail.com>
2020-12-05 22:44:37 +0100

    removing hardcore urls

--- scraper.py
+++ scraper.py
@@ -1,11 +1,15 @@
 from autoscraper import AutoScraper
 
-def prepare_scraper(url):
-    wanted_list = ["Claves para saber si una terraza de invierno debe estar al 40% o al 50% del aforo"]
+def prepare_scraper(url, wanted_list):
     scraper = AutoScraper()
     result = scraper.build(url, wanted_list)
     return scraper
 
 def scraping(url, scraper):
     results = scraper.get_result_similar(url)
-    print(results)
+    return results
+
+
+
+
+
