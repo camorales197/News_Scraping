b8fd942cec7809c283add08ccfdf762b138bff3f
Carlos <carlos_cc_13@hotmail.com>
2020-12-05 22:49:02 +0100

    adding logging

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from scraper import *
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Starting the script')
 
 url_train = 'https://www.elperiodicoextremadura.com/noticias/extremadura/claves-saber-si-terraza-invierno-debe-estar-' \
       '40-50-aforo_1260164.html'
@@ -6,7 +10,13 @@ url_train = 'https://www.elperiodicoextremadura.com/noticias/extremadura/claves-
 url1 = 'https://www.elperiodicoextremadura.com/noticias/extremadura/' \
        'como-funcionan-nuevos-test-covid-farmaciaseuros_1260171.html'
 
+wanted_list = ["Claves para saber si una terraza de invierno debe estar al 40% o al 50% del aforo"]
+
 
 if __name__ == '__main__':
-    scraper = prepare_scraper(url_train)
+    logging.debug('Starting "prepare_scraper"')
+    scraper = prepare_scraper(url_train, wanted_list)
+    logging.debug('Starting "scraping"')
     scraping(url1, scraper)
+    logging.debug('Script finished')
+
