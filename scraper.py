6d29382dd4bdb345f0baabfaf66a68e3cb6e1513
Carlos <camorales@outlook.com>
2020-12-07 00:43:33 +0100

    correcting linter

--- scraper.py
+++ scraper.py
@@ -1,15 +1,10 @@
-
 def prepare_scraper(url, wanted_list):
     from autoscraper import AutoScraper
     scraper = AutoScraper()
-    result = scraper.build(url, wanted_list)
+    scraper.build(url, wanted_list)
     return scraper
+
 
 def scraping(url, scraper):
     results = scraper.get_result_similar(url)
     return results
-
-
-
-
-
--- test_scraper.py
+++ test_scraper.py
@@ -1,21 +1,26 @@
-# to run the test write pytest on the terminal
+from scraper import prepare_scraper, scraping
 
-from scraper import *
 
-url_train = 'https://www.elperiodicoextremadura.com/noticias/extremadura/claves-saber-si-terraza-' \
+url_train = 'https://www.elperiodicoextremadura.com/noticias' \
+            '/extremadura/claves-saber-si-terraza-' \
             'invierno-debe-estar-40-50-aforo_1260164.html'
-url1 = 'https://www.elperiodicoextremadura.com/noticias/extremadura/como-funcionan-nuevos-test-' \
+url1 = 'https://www.elperiodicoextremadura.com/noticias/' \
+       'extremadura/como-funcionan-nuevos-test-' \
        'covid-farmaciaseuros_1260171.html'
-wanted_list = ["Claves para saber si una terraza de invierno debe estar al 40% o al 50% del aforo"]
+wanted_list = ["Claves para saber si una terraza de i"
+               "nvierno debe estar al 40% o al 50% del aforo"]
 
 scraper = prepare_scraper(url_train, wanted_list)
+
 
 def test_prepare_scrapper(url=url_train, wanted_list=wanted_list):
     scraper = prepare_scraper(url, wanted_list)
     result = scraper.build(url, wanted_list)
-    assert result == ['Claves para saber si una terraza de invierno debe estar al 40% o al 50% del aforo']
+    assert result == ['Claves para saber si una terraza '
+                      'de invierno debe estar al 40% o al 50% del aforo']
+
 
 def test_scraping(url=url1, scraper=scraper):
     results = scraping(url, scraper)
-    assert results == ['¿Cómo funcionan los nuevos test covid de las farmacias?']
-
+    assert results == ['¿Cómo funcionan los nuevos '
+                       'test covid de las farmacias?']
